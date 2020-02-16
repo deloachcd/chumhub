66da05fca641df1f4800e1911e7a3acb227350bb
Chandler DeLoach <deloach.cdd@gmail.com>
2020-02-16 15:25:28 -0700

    MVP motherfuckerrrrrrrrrrrrrr

--- websrc/populator.js
+++ websrc/populator.js
@@ -1,8 +1,10 @@
 // global constants
 let remote_metafile = "https://deloachcd.github.io/websrc/meta.json"
+let global_collection = null
 let albums_table = document.getElementById("albums");
 let sidenav = document.getElementById("sidenav");
 let sidenav_tags_list = []
+let visible_tags = []
 let table_count = 0
 let table_maxwidth = 5
 
@@ -16,7 +18,7 @@ function sidenav_create_tag(tagliteral) {
     checkbox = document.createElement("input")
     checkbox.setAttribute("type", "checkbox");
     checkbox.setAttribute("name", `checkbox_${tagliteral}`);
-    checkbox.setAttribute("value", false);
+    checkbox.addEventListener("change", renderFromCheckboxChange);
     label = document.createElement("label");
     label.setAttribute("for", `checkbox_${tagliteral}`);
     label.appendChild(document.createTextNode(`${tagliteral}`))
@@ -60,6 +62,29 @@ function renderAlbum(collection_entry) {
     table_count++;
 }
 
+function renderVisible() {
+    global_collection.forEach((entry, index) => {
+        // render entry in collection if all visible tags are matched
+        let render_entry = true;
+        visible_tags.forEach(tag => {
+            if (entry.tags.indexOf(tag) == -1) {
+                render_entry = false;
+                return;
+            }
+        })
+        if (render_entry) {
+            renderAlbum(entry);
+        }
+    })
+    /* if table has at least one entry and less than 5 rows,
+       pad it with empty cells */
+    if (albums_table.rows.length > 0) {
+        while (albums_table.rows[0].cells.length < 5) {
+            albums_table.rows[0].appendChild(document.createElement("td"));
+        }
+    }
+}
+
 function initial() {
     fetch(remote_metafile)
         .then((response) => {
@@ -73,7 +98,32 @@ function initial() {
                 // Render every album (we don't care about tags yet)
                 renderAlbum(entry);
             })
+            global_collection = collection
         });
+}
+
+function renderFromCheckboxChange() {
+    // event listener for checkboxes
+    var tag_to_modify = this.name.slice(checkbox.name.indexOf("_")+1);
+    if (this.checked) {
+        visible_tags.push(tag_to_modify)
+    } else {
+        var tagindex = visible_tags.indexOf(tag_to_modify);
+        visible_tags.splice(tagindex, 1);
+    }
+    renderFromVisibleTags();
+}
+
+function renderFromVisibleTags() {
+    // drop rows from old albums table
+    table_rows = Array.from(albums_table.rows)
+    table_rows.forEach(row => {
+        albums_table.removeChild(row);
+    })
+    // reset count
+    table_count = 0
+    // re-render
+    renderVisible();
 }
 
 initial();
